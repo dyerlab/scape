f30f1fb48d90a05f82ce374f92407f1c78fbf321
dyerlab <rjdyer@vcu.edu>
2019-07-16 10:49:42 -0400

    final changes to test-strata_centroids

--- tests/testthat/test-strata_centroids.R
+++ tests/testthat/test-strata_centroids.R
@@ -9,15 +9,18 @@ test_that("missing data", {
   names(data)[1] <- "Population"
 
   expect_that( strata_centroids(data), throws_error() )
-  data$Latitude <- c(26.4, 26.6, 32.7, 33.4)
+  data$Latitude <- c(26.4, 26.6, 32.6, 33.8)
 
   expect_that( strata_centroids(data), throws_error() )
-  data$Longitude <- c(-77.3, -77.6, -84.2, -86.3)
+  data$Longitude <- c(-77.3, -77.5, -84.2, -86.4)
 
   df <- strata_centroids(data)
 
-
   expect_true( is.data.frame(df) )
 
+  expect_true( df$Longitude[1] == mean(data$Longitude[1:2]) )
+  expect_true( df$Longitude[2] == mean(data$Longitude[3:4]) )
+  expect_true( df$Latitude[1] == mean(data$Latitude[1:2]) )
+  expect_true( df$Latitude[2] == mean(data$Latitude[3:4]) )
 
 })
